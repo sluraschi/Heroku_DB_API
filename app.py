16b68041f125dd38dea23cedf015c5747d8b6639
sebastian luraschi <sebastian.luraschi8@gmail.com>
2021-08-09 19:14:58 -0300

    new example case mid way

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
-from flask import Flask, request, Response, jsonify
+from flask import Flask, request, Response
 import psycopg2
 
-from model import package, seismic_payload
+from model import package
 import db_credentials
 import db_operations
 import constants
 
 app = Flask(__name__)
+
+
+@app.route('/example', methods=['POST'])
+def upload_example():
+    if not request.json:
+        return Response(response="{'error': 'Missing package'}", status=400)
+    try:
+        pack = package.Package(constants.PayloadTypes.temp_and_hum.name, **request.json['package']) # ToDo
+        credentials = db_credentials.DbCredentials(**request.json['Db_connection'])
+    except TypeError as e:
+        return Response(response="malformed http body", status=400)
+
+    # Establish connection
+    try:
+        session_instance = db_operations.DbSession(credentials, constants.TEMPERATURE_DATABASE_HOST, constants.TEMPERATURE_DATABASE)    # ToDo
+    except (Exception, psycopg2.Error) as error:
+        print("Database authentication failed")
+        return Response(response="Database authentication failed", status=401)
+
+    try:
+        session_instance.upload_package(pack)
+    except (Exception, psycopg2.Error) as error:
+        print("Failed to insert records with error:", error)
+        return Response(response=f"Failed to insert records with error: {error}", status=400)
+    return Response(response="Package stored successfully", status=200)
 
 
 @app.route('/temp-hum', methods=['POST'])
@@ -60,7 +85,6 @@ def upload_seismic_package():
     return Response(response="Package stored successfully", status=200)
 
 
-
 @app.route('/ultrasound', methods=['POST'])
 def upload_ultrasound_package():
     if not request.json:
@@ -85,16 +109,6 @@ def upload_ultrasound_package():
         print("Failed to insert records with error:", error)
         return Response(response=f"Failed to insert records with error: {error}", status=400)
     return Response(response="Package stored successfully", status=200)
-
-# How to return a class as JSON
-@app.route('/seismic-data/payload/<id_to_retrieve>', methods=['GET'])
-def get_payload_by_id(id_to_retrieve):
-    if not id_to_retrieve:
-        return Response(response="{'error': 'Bad id'}", status=404)
-
-    # todo: Esto es un SELECT de la base de datos
-    j = jsonify({"ping": "pong"})
-    return j
 
 
 @app.route('/')
---
+++ model/example_payload.py
@@ -0,0 +1,39 @@
+from enum import Enum
+from model.payload import Payload
+
+EXAMPLE_TABLE_NAME = "measurement"  # (1) Nueva constante con el nombre de la tabla
+
+
+def parse_payload(payload): # (2) Como dijimos, el payload es: DXXTXX, aca lo parseo a 2 variables separadas
+    if payload == "FAIL":
+        d_measure = None
+        t_measure = None
+    else:
+        d_position = payload.upper().index("D")
+        t_position = payload.upper().index("T")
+        t_measure = payload[t_position + 1:]
+        d_measure = payload[d_position + 1:t_position]
+    return [d_measure, t_measure]
+
+
+class ExamplePayload(Payload):  # (3) Aqui se indica la herencia de la clase Payload
+
+    def __init__(self, payload):  # (4) En el mismo constructor se indican los atributos de la clase y se parsea el paylaod
+        self.distance, self.temperature = parse_payload(payload)
+
+    def to_dict(self):  # (5) Devuelve de la forma {tag1: atributo1, tag2: atributo2, ...}
+        return {ExamplePayloadTags.distance.name: self.distance,
+                ExamplePayloadTags.temperature.name: self.temperature}
+
+    def to_tuple(self): # (6) Devuelve de la forma (atributo1, atributo2, ...)
+        return (self.distance,
+                self.temperature)
+
+    def get_insert_query(self): # (7) Nuevo metodo con la query para insertar en la base de datos
+        return f"INSERT INTO {EXAMPLE_TABLE_NAME} (package_id, segment_id, date," \
+               f"position, temperature) VALUES (%s,%s)"
+
+
+class ExamplePayloadTags(Enum): # (8) Los tags para usar en el diccionario y no manejar stings que llevan a errores humanos
+    distance = 1
+    temperature = 2
